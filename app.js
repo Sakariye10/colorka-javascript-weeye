ee4a6be9e80dd9f0182a4a2fb36bbb3fbb9c063d
sakariye10 <sakariyesaeed51@gmail.com>
2022-10-10 09:42:38 +0300

    waa code js color

---
+++ app.js
@@ -0,0 +1,19 @@
+// const pras = document.querySelector(".hello");
+// pras.innerHTML = 23;
+// pras.style.color = "blue";
+// // console.log(pras);
+// const pp = Document.querySelector("button");
+// pp.innerHTML = "ssss";
+
+// console.log(pp);
+
+// const h1 = document.querySelector(".hello");
+// const btn1 =Document.querySelector(".btn");
+
+// const changecolor = Document.getElementById('.box')
+
+// function changecolor(id) {
+//   document.body.style = document.getElementById(id).innerHTML;
+// }
+// consol.log(changecolor);
+// document.body.style.background = Document.querysalactorAll(id).innerHTML;
